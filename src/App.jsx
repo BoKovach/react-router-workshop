5f191d082e2e5e3f744645ff722fbf0a345c7210
Bo Kovach <bo.kovachh@gmail.com>
2025-02-22 15:54:13 +0200

    Add header nav bar to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Button from '@mui/material/Button';
+import Header from './components/header';
 
 function App() {
   return (
     <>
-      <h1>React Router Workshop</h1>
-      <Button variant="contained">Hello world</Button>
+        <Header />
     </>
   )
 }
